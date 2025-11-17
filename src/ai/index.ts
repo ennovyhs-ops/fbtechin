418d218b7f52af2d4b5a2975afbf9529f32df261
H S <ennovyhs@gmail.com>
2025-11-17 00:56:40 +0000

    use the latest available gemini model and since it was working before th

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -2,7 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({
-    model: 'gemini-pro',
-  })],
+  plugins: [googleAI()],
+  model: 'gemini-1.5-flash-latest',
 });
